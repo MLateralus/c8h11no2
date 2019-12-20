6225be95105a922335f9bb395e6e685d8ec66414
Lateralus <michal.czerwien@gmail.com>
2019-12-20 13:49:23 +0100

    MC Bart to jest zią

--- server/server.js
+++ server/server.js
@@ -1,41 +1,39 @@
 // Externals
 const srv = require('./options');
+const fs = require('fs');
 const express = require('express');
-const request = require('request');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 const port = srv.conf.port;
-
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: false,
 }));
+const server = require('http').createServer(app);
+const io = socketIO(server);
 
-app.get('/index', async (req, res) => {
-	let rss = req.query.url;
-	res.json({"err": `hehehehehe`});
+var timeInMss = 
+
+io.on('connection', (socket) => {
+    socket.on('Bart', (message) => {
+        // Poszukaj Bartka, 
+        // Znajdź Bartka
+        // Odpowiedz Bartek to MC
+        var result = `message: Bartek to jest ziooom`;
+        io.emit('BartekResponse', result);
+    });
+
+    socket.on('Photo', (data) => {
+        fs.writeFile(`./img/${Date.now()}.png`, Buffer(data, 'base64'), err => {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+    });
 });
 
-
-function getRSS(url){
-	return new Promise((res, rej) => {
-		request({
-			url: url,
-			json: true,
-			headers: {
-				'User-Agent': 'Kjeragbolten'
-			}
-		},
-		function(error, resp, body){
-			if(!error && resp.statusCode === 200){
-				res(resp.body);
-			} else {
-				rej(`Unable to resolve url: ${url}, please check if the url is correct`);
-			}
-		});
-	});
-}
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('info', `c8h11no2 up on: ${port}`);
 });
